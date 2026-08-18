8d006956b2437797da8ba1268f2b8476bbe5d241
unknown <cesaroliva.work@gmail.com>
2026-08-18 10:40:33 -0600

    getting product id from params and adding dependencies (Confetti)

--- src/pages/Catalogo.tsx
+++ src/pages/Catalogo.tsx
@@ -1,7 +1,85 @@
+export const productos = [
+    {
+        id: 1,
+        nombre: "Mouse Gamer inalambrico" ,
+        precio: 150,
+        descripcion: "Mouse gamer con luces rgb y sensibilidad",
+        imagen: "https://m.media-amazon.com/images/I/61QY3V6A-NL.jpg"
+    },
+    {
+        id: 2,
+        nombre: "Audifonos inalambricos" ,
+        precio: 200,
+        descripcion: "Audifonos inalambricos con larga duracion de bateria",
+        imagen: "https://m.media-amazon.com/images/I/71BFvz7N32L._AC_.jpg" 
+    },
+     {
+        id: 3,
+        nombre: "Cargador Samsung 65w" ,
+        precio: 300,
+        descripcion: "Cargador de carga super rapida",
+        imagen: "https://www.macysdigital.com/wp-content/uploads/2025/01/Samsung-Cargador-Trio-65W-Negro.png"
+    },
+     {
+        id: 4,
+        nombre: "Cargador Laptop 30w" ,
+        precio: 200,
+        descripcion: "Cargador de carga rapida",
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_948355-MLV75911408960_042024-O.webp"
+    },
+     {
+        id: 5,
+        nombre: "Reloj inteligente" ,
+        precio: 999,
+        descripcion: "Reloj inteligente con conexion satelital",
+        imagen: "https://m.media-amazon.com/images/I/81MwG9dLIYL._AC_.jpg"
+    },
+     {
+        id: 6,
+        nombre: "Teclado inalambrico" ,
+        precio: 599,
+        descripcion: "Teclado inalambrico con luces led",
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_931030-MLM73621179176_122023-O.webp"
+    },
+     {
+        id: 7,
+        nombre: "Audifonos Gamer" ,
+        precio: 399,
+        descripcion: "Audifonos alambricos gamer con sonido envolvente",
+        imagen: "https://th.bing.com/th/id/R.03317460c1ddb5d64ef13cb912288390?rik=82ZKl3s97bUAGg&pid=ImgRaw&r=0"
+    },
+     {
+        id: 8,
+        nombre: "Cargador plugin" ,
+        precio: 100,
+        descripcion: "Cargador para coche de carga rapida",
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_882301-MCO70413434914_072023-O.webp"
+    },
+     {
+        id: 9,
+        nombre: "teclado y mouse gamer" ,
+        precio: 650,
+        descripcion: "teclado con mouse inalambrico gamer",
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_633025-MLU78555066256_082024-O.webp"
+    },
+    {
+        id: 10,
+        nombre: "usb 1tb" ,
+        precio: 899,
+        descripcion: "usb con 1tb de almacenamiento",
+        imagen: "https://th.bing.com/th/id/R.68ca04f058267945a0cc0de6f20e33c5?rik=sw%2bL4FdyclEL0Q&pid=ImgRaw&r=0"
+    }
+]
+
 const Catalogo = () => {
-    return (
-        <>Catalogo</>
+    return ( 
+        <>
+            {productos}
+            {productos.map((product)=>{
+                <h2>{product.id}</h2>
+            })}
+        </>
     );
 }
  
-export default Catalogo;
+export default Catalogo ; 
--- src/pages/Producto.tsx
+++ src/pages/Producto.tsx
@@ -1,7 +1,99 @@
-const Producto = () => {
+import { Link, useParams } from "react-router-dom";
+import { productos } from "./Catalogo";
+import ReactConfetti from "react-confetti";
+import { useState } from "react";
+
+function Producto() {
+    const { productId } = useParams();
+    const [prefix, value] = productId?.split("-") ?? [];
+
+    const id = prefix === "producto" && /^\d+$/.test(value)
+        ? Number(value)
+        : null;
+        
+    const product = productos.find(producto => producto.id === id);
+
+    const [showConfetti, setShowConfetti] = useState(false);
+
+    const handleComprar = () => {
+        setShowConfetti(true);
+
+        setTimeout(() => {
+            setShowConfetti(false);
+        }, 5000);
+    };
+
+    if (!product) {
+        return (
+            <main className="mx-auto flex min-h-screen max-w-4xl flex-col px-8 justify-center">
+                <h2 className="text-5xl font-semibold mb-4 text-center">
+                    Producto no encontrado
+                </h2>
+
+                <p className="text-lg font-light mb-8 text-center">
+                    El producto que buscaste no fue encontrado.
+                </p>
+
+                <section className="flex justify-center">
+                    <Link
+                        to="/"
+                        className="group flex items-center max-w-80 justify-center text-center text-lg gap-2 cursor-pointer bg-neutral-900 hover:bg-neutral-800 py-2 px-6 rounded-lg"
+                    >
+                        Volver a Inicio
+                    </Link>
+                </section>
+            </main>
+        );
+    }
+
     return (
-        <>Producto Individual</>
+        <main className="mx-auto flex min-h-screen max-w-4xl flex-col gap-8 px-8 md:justify-center mt-4 md:mt-12 mb-24">
+
+            {showConfetti && (
+                <ReactConfetti
+                    width={window.innerWidth}
+                    height={window.innerHeight}
+                    recycle={false}
+                    numberOfPieces={500}
+                />
+            )}
+
+            <div className="grid grid-cols-1 gap-8 md:grid-cols-2 pb-8">
+                <img
+                    loading="lazy"
+                    src={product.imagen}
+                    alt={product.nombre}
+                    className="rounded-2xl bg-neutral-900 block w-full object-contain transition-all duration-300"
+                />
+
+                <section className="flex flex-col justify-center">
+                    <span className="text-xs uppercase tracking-[0.35em] text-neutral-400 mb-4 block">
+                        Detalles del producto
+                    </span>
+
+                    <h1 className="text-5xl font-semibold mb-4">
+                        {product.nombre}
+                    </h1>
+
+                    <p className="text-3xl font-light mb-2">
+                        ${product.precio}
+                    </p>
+
+                    <p className="text-lg font-light mb-2">
+                        {product.descripcion}
+                    </p>
+
+                    <button
+                        type="button"
+                        onClick={handleComprar}
+                        className="text-lg gap-2 cursor-pointer bg-neutral-900 hover:bg-neutral-800 py-2 px-6 rounded-lg"
+                    >
+                        Comprar
+                    </button>
+                </section>
+            </div>
+        </main>
     );
 }
- 
+
 export default Producto;
